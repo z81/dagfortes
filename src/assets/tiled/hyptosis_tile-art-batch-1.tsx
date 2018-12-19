5fc1aae95b20e6b1adbc683aa0c762e17cbfe3fc
z80 <muzo0@yandex.ru>
2018-12-19 21:30:48 +0300

    upd map layers

---
+++ src/assets/tiled/hyptosis_tile-art-batch-1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="hyptosis_tile-art-batch-1" tilewidth="32" tileheight="32" tilecount="900" columns="30">
+ <image source="../tiles/hyptosis_tile-art-batch-1.png" width="960" height="960"/>
+</tileset>
---
+++ src/assets/tiled/hyptosis_tile-art-batch-5.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="hyptosis_tile-art-batch-5" tilewidth="32" tileheight="32" tilecount="722" columns="19">
+ <image source="../Downloads/hyptosis_tile-art-batch-5.png" width="636" height="1219"/>
+</tileset>
