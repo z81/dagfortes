c045f7937d3864e36f568f8eb34cd53d92bf12bc
z80 <muzo0@yandex.ru>
2018-12-19 21:33:53 +0300

    rm old map

--- src/assets/tiled/hyptosis_tile-art-batch-4.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="hyptosis_tile-art-batch-4" tilewidth="32" tileheight="32" tilecount="900" columns="30">
- <image source="../Downloads/hyptosis_tile-art-batch-4.png" width="960" height="960"/>
-</tileset>
